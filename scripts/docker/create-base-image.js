b924477e34959790a117c1b3384f4ba07d937f5c
Paweł Trysła <pawel.trysla@gmail.com>
2017-05-17 23:20:13 +0200

    use execSync

--- scripts/docker/create-base-image.js
+++ scripts/docker/create-base-image.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-const crossSpawn = require('cross-spawn');
+const { execSync } = require('child_process');
 const version = require('../../lerna.json').version.replace('v', '');
 
 const tag = `truecar/gluestick:${version}`;
 console.log(`Creating Docker base image for ${tag}`);
 
-module.exports = (spawn = crossSpawn) => {
+module.exports = () => {
   console.log('Building docker image...');
-  console.log(spawn('docker', [
+  console.log(execSync([
+    'docker',
     'build',
     '-f', './scripts/docker/Dockerfile',
     '--force-rm=true',
     '-t', tag,
     '--build-arg', `GLUESTICK_VERSION=${version}`,
     '.',
-  ], { stdio: 'inherit', env: Object.assign({}, process.env) }));
+  ].join(' '), { stdio: 'inherit', env: Object.assign({}, process.env) }));
 
   console.log('Pushing image to Docker Hub...');
-  spawn('docker', ['push', tag], { stdio: 'inherit', env: Object.assign({}, process.env) });
+  execSync(`docker push ${tag}`, { stdio: 'inherit', env: Object.assign({}, process.env) });
 };
