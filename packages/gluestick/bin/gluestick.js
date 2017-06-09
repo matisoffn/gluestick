8f816c02ccc4a3390cdc1087a53b5501e73c3f62
Paweł Trysła <pawel.trysla@gmail.com>
2017-06-09 11:57:23 +0200

    add npm@5 check to gluestick binary file

--- packages/gluestick/bin/gluestick.js
+++ packages/gluestick/bin/gluestick.js
@@ -1,6 +1,22 @@
 #!/usr/bin/env node
 
-if (!__dirname.startsWith(process.cwd())) {
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * With npm@4 it should be:
+ *   __dirname     /Users/<userName></userName>/Documents/<gluestickApp>/node_modules/gluestick
+ *   process.cwd() /Users/<userName></userName>/Documents/<gluestickApp>
+ * but with npm@5 it will be installed in global cache and symlinked to project directory,
+ * so we just need to check if the `node_modules/gluestick` directory in currect project
+ * is a symlink.
+ */
+const isInstalledGlobally = (
+  !__dirname.startsWith(process.cwd()) && // npm@4 check
+  !fs.lstatSync(path.join(process.cwd(), 'node_modules/gluestick')).isSymbolicLink() // npm@5 check
+);
+
+if (isInstalledGlobally) {
   const red = require('chalk').red;
   console.log(red('It looks like you\'ve installed the `gluestick` package globally,'));
   console.log(red('but `gluestick` is meant to be installed locally as a dependency.'));
