106a75f0479756ec5afaa79135ad2410ad029734
Paweł Trysła <pawel.trysla@gmail.com>
2017-08-11 15:23:55 +0200

    remove obsolete export from shared index.js

--- packages/gluestick/shared/index.js
+++ packages/gluestick/shared/index.js
@@ -3,7 +3,6 @@ export BodyAttributes from './components/BodyAttributes';
 export withDataLoader from './hoc/withDataLoader';
 export redirect from './hoc/redirect';
 export createStore from './lib/createStore';
-export * from './lib/routeEvents';
 export * from './lib/constants';
 export getHttpClient from './lib/getHttpClient';
 export { set404StatusCode, setStatusCode } from './lib/actions';
